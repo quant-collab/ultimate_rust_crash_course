ab5510af3da6f6f1894c1a52cd5f283930cd0fca
Jacob Quant <jacobq@gmail.com>
2022-05-04 06:32:45 -0500

    Refactor Exercise 1, just for fun

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,10 +1,26 @@
 const STARTING_MISSILES: i32 = 8;
-const READY_AMOUNT: i32 = 2;
+
+fn fire(n: i32, available: &mut i32) {
+    if n < 1 {
+        println!("Can't fire less than 1 missile, silly");
+        return;
+    }
+    if n < *available {
+        println!("Firing {} of my {} missiles...", n, available);
+        *available -= n;
+    } else if *available > 0 {
+        println!("Request to fire {} missiles but only have {} missiles (firing those)", n, available);
+        *available = 0;
+    } else {
+        println!("Can't fire {} missiles because we're out of ammo :'(", n);
+    }
+}
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
-    missiles -= ready;
-    println!("Firing {} of my {} missiles...", ready, missiles);
-    println!("{} missiles left", missiles);
+    let mut available: i32 = STARTING_MISSILES;
+    fire(-9, &mut available);
+    fire(4, &mut available);
+    fire(1, &mut available);
+    fire(100, &mut available);
+    fire(2, &mut available);
 }
